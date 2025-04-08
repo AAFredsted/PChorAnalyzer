7fb9121282e64469eefb8db6a7746aaab7c84b25
AAFredsted <Andra_Fredsted@proton.me>
2025-04-08 22:06:34 +0200

    feat: setup of project

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "./utils/Utils.hpp"
+#include "./pchor/pchor.hpp"
+#include "./analyzer/Analyzer.hpp"
+#include "./analyzer/AstVisitor.hpp"
+#include <print>
+
+int main(){
+    std::println("Hello World");
+}
---
+++ src/pchor/pchor.cpp
@@ -0,0 +1,1 @@
+#include "pchor.hpp"
