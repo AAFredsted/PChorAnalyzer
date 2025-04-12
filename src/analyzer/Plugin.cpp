f7f33b4688667f9e43298f632e49cd225b294f02
AAFredsted <Andra_Fredsted@proton.me>
2025-04-12 13:55:50 +0200

    feat: made working cmake setup

---
+++ src/analyzer/Plugin.cpp
@@ -0,0 +1,6 @@
+#include "../utils/Utils.hpp"
+#include "../pchor/pchor.hpp"
+#include "./Analyzer.hpp"
+#include "./AstVisitor.hpp"
+
+#include "clang/Frontend/FrontendPluginRegistry.h"
--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "./utils/Utils.hpp"
-#include "./pchor/pchor.hpp"
-#include "./analyzer/Analyzer.hpp"
-#include "./analyzer/AstVisitor.hpp"
-#include <print>
-
-int main(){
-    std::println("Hello World");
-}
