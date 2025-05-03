cbb00c49720f8ec2d524a594e99bd87109573820
AAFredsted <Andra_Fredsted@proton.me>
2025-05-03 23:41:03 +0200

    feat: improved function detection matcher

--- test/test.cpp
+++ test/test.cpp
@@ -1,14 +1,45 @@
 #include <print>
+#include <string>
+#include <vector>
 
+struct Message {
+    explicit Message(): str("") {}
+    std::string str;
+};
 
 class Anne {
+public:
+    Message* msg;
+    
+    explicit Anne(): msg() {}
+    
+    Message getMessage(){
+        return *msg;
+    }
+
+    void dosomething() {
+        msg->str = "hello";
+    }
 
 };
 
 class Nicholas {
+public:
+    const Message* msg;
+
+    explicit Nicholas(): msg() {}
+
+    Message getMessage(){
+        Message a{};
+        return *msg;
+    }
+
 
 };
 
+
 int main(){
+
+
     std::println("Hello Worlds");
 }
